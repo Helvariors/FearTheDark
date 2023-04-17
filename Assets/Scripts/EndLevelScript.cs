62cc6c618c448b2a47da3bc31675a54b41f27a80
HWSxDejv <davedinox@gmail.com>
2023-04-17 20:12:41 +0200

    Finished first map and Added code for loading Sceens

---
+++ Assets/Scripts/EndLevelScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelScript : MonoBehaviour
+{
+	BoxCollider2D EndLevelTrigger;
+	void Start()
+	{
+		EndLevelTrigger = gameObject.GetComponent<BoxCollider2D>();
+	}
+
+	void OnTriggerEnter2D()
+	{
+		Debug.Log("invoke LoadEndScene");
+		Invoke("LoadEndScreen", 2f);
+	}
+	void LoadEndScreen()
+	{
+		 Debug.Log("LoadEndScene");
+		 SceneLoader.Load(SceneLoader.Scene.EndScreen);
+	}
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public static class SceneLoader
 {
-    public enum Scene
-    {
-        SampleScene,
-    }
-    public static void Load(Scene scene)
-    {
-        SceneManager.LoadScene(scene.ToString());
-    }
+	public enum Scene
+	{
+		MainMenu, 
+		EndScreen, 
+		SampleScene
+	}
+	public static void Load(Scene scene)
+	{
+		Debug.Log("LoadScene");
+		SceneManager.LoadScene(scene.ToString());
+		Debug.Log("Loaded scene: " + scene.ToString());
+	}
 }
